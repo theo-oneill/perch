ea998b37c0cf502b86b6b6017851276bb0b2d47f
Eric Koch <koch.eric.w@gmail.com>
2025-11-18 09:02:26 -0500

    Add string and version to init for local builds

--- perch/__init__.py
+++ perch/__init__.py
@@ -1,1 +1,7 @@
-# perch/__init__.py
+"""perch is a python package for applying the topological technique of persistent homology to
+images & cubes.  Persistent Homology identifies "holes" of different dimensions in data.
+In the perch framework, this enables the identification, segmentation,
+and hierarchical mapping of multi-scale structures in astronomical data.
+"""
+
+__version__ = '0.0.1'
