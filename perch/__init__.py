9530ef26b6137110cadfdfade824e5f403f7c35e
Eric Koch <koch.eric.w@gmail.com>
2026-01-08 15:15:41 -0500

    setuptools build now working

--- perch/__init__.py
+++ perch/__init__.py
@@ -1,7 +1,11 @@
-"""perch is a python package for applying the topological technique of persistent homology to
+'''
+perch is a python package for applying the topological technique of persistent homology to
 images & cubes.  Persistent Homology identifies "holes" of different dimensions in data.
 In the perch framework, this enables the identification, segmentation,
 and hierarchical mapping of multi-scale structures in astronomical data.
-"""
+'''
 
-__version__ = '0.0.1'
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
